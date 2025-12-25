cfecdd4f4cfe0ca943c19e27fb1f5d237ef4566e
dhruvit <dhruvitgarthiya@gmail.com>
2025-12-25 15:11:51 +0530

    this is how we should write json parsing in typescript

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,9 @@
-const data = JSON.parse('{ "name": "dhruvit","age" : "20" }');
+interface User {
+  name: string;
+  age: string;
+}
+
+const data: User = JSON.parse('{"name" : "dhruvit" , "age" : "20"}');
+
+console.log(data.name);
+// console.log(data.salary);
