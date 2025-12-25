55a241cfbcd36a4a11a56580dedf970697ec7c8e
dhruvit <dhruvitgarthiya@gmail.com>
2025-12-25 14:48:35 +0530

    first commit : started learning the typescript

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `hello ${person} , welcom to chaiandcode`;
+}
+const username = "chai aur code";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+function greet(person: string): string {
+  return `hello ${person} , welcom to chaiandcode`;
+}
+
+const username: string = "chai aur code";
+console.log(greet(username));
+
+
